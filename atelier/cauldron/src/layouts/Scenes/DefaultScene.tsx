4c3b2b697fa70bb01505c45878e0d4cbe4054a52
Ben-Swindells <bensterbas@gmail.com>
2024-02-02 00:05:35 +0000

    fix: reworked core infrastructure to make ready for build

--- atelier/cauldron/src/layouts/Scenes/DefaultScene.tsx
+++ atelier/cauldron/src/layouts/Scenes/DefaultScene.tsx
@@ -1,11 +1,13 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stage } from "@react-three/drei";
 
 export const DefaultScene = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <ambientLight intensity={1} />
-      <OrbitControls minZoom={1} maxPolarAngle={Math.PI / 2} />
-      {children}
+      <OrbitControls minZoom={1} />
+      <Stage intensity={0} shadows={false}>
+        {children}
+      </Stage>
     </>
   );
 };
--- atelier/elements/scripts/Cameras/FollowCamera.tsx
+++ atelier/elements/scripts/Cameras/FollowCamera.tsx
@@ -1,7 +1,10 @@
+import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 
 type FollowCameraProps = {
   target: THREE.Vector3;
 };
 
-export const FollowCamera = ({ target }: FollowCameraProps) => {};
+export const FollowCamera = ({ target }: FollowCameraProps) => {
+  return <OrbitControls target0={target} />;
+};
