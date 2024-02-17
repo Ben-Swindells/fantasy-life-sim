66ea3daec984f9cb0276265c73c2de39080c924a
Ben-Swindells <bensterbas@gmail.com>
2024-02-17 23:28:27 +0000

    feat: overhaul of cauldron layout

--- atelier/cauldron/src/layouts/index.tsx
+++ atelier/cauldron/src/layouts/index.tsx
@@ -3,12 +3,12 @@ import { prefabPages } from "../pages";
 import { Canvas } from "@react-three/fiber";
 
 type PrefabLayoutProps = {
-  element: JSX.Element;
+  children: JSX.Element;
 };
 
-export const PrefabLayout = ({ element }: PrefabLayoutProps) => {
+export const PrefabLayout = ({ children }: PrefabLayoutProps) => {
   return (
-    <div className="w-screen h-screen bg-[#240329] flex">
+    <div className="w-screen h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex">
       <Menubar>
         {prefabPages.map((page, index) => {
           return (
@@ -21,8 +21,8 @@ export const PrefabLayout = ({ element }: PrefabLayoutProps) => {
         })}
       </Menubar>
       <div className="flex w-full h-full justify-center items-center flex-col">
-        <div className="w-3/4 bg-white aspect-video flex">
-          <Canvas shadows="soft">{element}</Canvas>
+        <div className="w-3/4  aspect-video flex shadow-2xl">
+          <Canvas shadows="soft">{children}</Canvas>
         </div>
       </div>
     </div>
