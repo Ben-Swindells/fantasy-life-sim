45c684f2e8839ad7992a736aa28b1a1e82f62c2e
Ben Swindells <bensterbas@gmail.com>
2024-02-26 13:33:13 +0000

    feat: added phyics

--- apps/elixira/src/renderer/src/Game.tsx
+++ apps/elixira/src/renderer/src/Game.tsx
@@ -1,13 +1,16 @@
 import { SandboxScene } from './components/scenes'
 import { GameGUI } from './engine/ui'
 import { GameWindow } from './engine/window'
+import { Physics } from '@react-three/rapier'
 
 function Game(): JSX.Element {
   return (
     <>
       <GameGUI />
       <GameWindow>
-        <SandboxScene />
+        <Physics debug={false}>
+          <SandboxScene />
+        </Physics>
       </GameWindow>
     </>
   )
