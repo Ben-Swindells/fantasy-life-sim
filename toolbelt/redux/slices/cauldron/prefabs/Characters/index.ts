19c934be084417cdb111649b39fae87b8bd1ff91
Ben-Swindells <bensterbas@gmail.com>
2024-03-04 23:32:52 +0000

    feat: redux work

--- toolbelt/redux/slices/cauldron/prefabs/Characters/index.ts
+++ toolbelt/redux/slices/cauldron/prefabs/Characters/index.ts
@@ -1,11 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface CharacterState {
-
+  transform: {
+    position: { x: number; y: number; z: number };
+    rotation: { x: number; y: number; z: number };
+    scale: { x: number; y: number; z: number };
+  };
 }
 
 const initialState: CharacterState = {
- 
+  transform: {
+    position: { x: 0, y: 0, z: 0 },
+    rotation: { x: 0, y: 0, z: 0 },
+    scale: { x: 1, y: 1, z: 1 },
+  },
 };
 
 export const characterSlice = createSlice({
