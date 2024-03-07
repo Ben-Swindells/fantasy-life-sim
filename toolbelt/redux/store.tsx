0a86ad86efc29ae4857f736a8845df0a9da89490
Ben-Swindells <bensterbas@gmail.com>
2024-03-07 23:19:37 +0000

    fix: disable serializing in middleware

--- toolbelt/redux/store.tsx
+++ toolbelt/redux/store.tsx
@@ -6,6 +6,10 @@ export const store = configureStore({
     game: gameReducer,
     cauldron: cauldronReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
