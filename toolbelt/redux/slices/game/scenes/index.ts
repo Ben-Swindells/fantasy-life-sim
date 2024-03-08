b8b9fbe52eca7715b39ebc143c7c962535359bf4
Ben-Swindells <bensterbas@gmail.com>
2024-03-08 21:36:58 +0000

    fix: add empty reducers

--- toolbelt/redux/slices/game/scenes/index.ts
+++ toolbelt/redux/slices/game/scenes/index.ts
@@ -1,10 +1,14 @@
-import { combineReducers, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, combineReducers, createSlice } from "@reduxjs/toolkit";
+
+const emptyReducer = (state = {}, action: PayloadAction) => state;
 
 // A place to add variables to all scenes
 const scenesSlice = createSlice({
   name: "scenes",
   initialState: {},
-  reducers: {},
+  reducers: {
+    emptySlice: emptyReducer,
+  },
 });
 
 // Add the scenes slices to the root reducer, world 1, world 2, etc.
